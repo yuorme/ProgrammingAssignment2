172075f4d21bbadd946ef1d9ae9ae55f1acba4ba
yuorme <yuorme@gmail.com>
2014-07-26 02:17:21 -0400

    Update cachematrix.R
    Editing comments for clarity

--- cachematrix.R
+++ cachematrix.R
@@ -11,22 +11,22 @@ makeCacheMatrix <- function(x = matrix()) {
         x <<- y #passes z$set(y) to makeCacheMatrix(x)
         m <<- NULL #resets the cached inverted matrix if z$set() is called
     }
-    get <- function() x #gets x from makeCacheMatrix(x) input
-    setMatrix <- function(invmatrix) m <<- invmatrix #sets m to the inverted matrix calculated in cacheSolve
+    get <- function() x #used by cacheSolve to obtain matrix x from makeCacheMatrix
+    setMatrix <- function(invmatrix) m <<- invmatrix #caches the inverted matrix calculated in cacheSolve as m in makeCacheMatrix
     getMatrix <- function() m #getMatrix function allows cacheSolve to obtain m from makeCacheMatrix
     list(set = set, get = get, setMatrix = setMatrix, getMatrix = getMatrix) #sets makeCacheMatrix as a list of functions
 }
 
 #cacheSolve - if no cached matrix exists, calculates the inverted matrix and stores it in cache
 
-cacheSolve <- function(x, ...) { #the ... allows us to pass arguments from cacheSolve to makeCacheMatrix
+cacheSolve <- function(x, ...) { #the ... allows arguments to pass from cacheSolve to makeCacheMatrix
     m <- x$getMatrix() #calls makeCacheMatrix$getMatrix which obtains m from the makeCacheMatrix function
     if(!is.null(m)) { #if cached matrix exists then return the cached matrix
         message("getting cached data")
-        return(m) #returns the cached matrix from makeCacheMatrix
+        return(m) #returns the cached matrix from m in makeCacheMatrix
     }
-    data <- x$get() #calls makeCacheMatrix$get to obtain the input argument makeCacheMatrix(x)
+    data <- x$get() #calls makeCacheMatrix$get to obtain the input matrix x from makeCacheMatrix
     m <- solve(data, ...) # returns a matrix that is the inverse of 'x'
-    x$setMatrix(m) #calls makeCacheMatrix$setMatrix
+    x$setMatrix(m) #calls makeCacheMatrix$setMatrix to cache the inverted matrix
     m #prints m
 }
